56a9ab820de442658d9b002ee97b4f75f6fde827
Наиль Загидулин <zagidulin.nf@esphere.ru>
2021-10-21 17:31:00 +0300

    Add CarWashServiceClientV1Controller test and fix configuration with controller

--- carwash/src/main/java/online/carwashservice/carwash/dto/request/EnableServiceRq.java
+++ carwash/src/main/java/online/carwashservice/carwash/dto/request/EnableServiceRq.java
@@ -1,8 +1,12 @@
 package online.carwashservice.carwash.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EnableServiceRq {
     private boolean enable;
 }
